8f231d26147db14528323d197059404c6513f49b
Krish Jotaniya <jotaniyakrish07@gmail.com>
2022-10-16 20:50:43 +0530

    confetti added

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import "./Styles/Home.css";
 import "./Styles/Animation.css";
 import "animate.css";
+import ConfettiExplosion from "react-confetti-explosion";
 const Home = () => {
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState("-");
   function updateTimer() {
     let future = Date.parse("November 19, 2022 03:00:00");
     let now = new Date();
@@ -15,6 +16,13 @@ const Home = () => {
     updateTimer();
   }, 1000);
 
+  const bigExplodeProps = {
+    force: 0.4,
+    duration: 3000,
+    particleCount: 60,
+    height: 1200,
+    width: 1200,
+  };
   return (
     <>
       <div className="page-bg"></div>
@@ -25,13 +33,15 @@ const Home = () => {
         <div className="particle particle-4"></div>
       </div>
       <div className="headingArea">
-        <p className="getReadyText">Get Ready For</p>
-        {/* <p className="getReadyText">By IOT SENIORS</p> */}
+        {date !== "-" && <ConfettiExplosion {...bigExplodeProps} />}
+        <p className="getReadyText">IOT Seniors Presents</p>
         <p className="homePageTitle animate__animated animate__fadeIn">
           {/* COMING SOON */}
           Reverie'22
         </p>
-        <p className="seniorText">{date} Days To Go</p>
+        <p className="seniorText animate__animated animate__fadeIn">
+          {date} Days To Go
+        </p>
       </div>
     </>
   );
